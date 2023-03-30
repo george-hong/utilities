f7f20e2cf8c61500c7e8d7960b62c36ce6c8fee3
george-hong <hcj147@vip.qq.com>
2023-03-30 23:14:49 +0800

    other: remove useless file

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,3 @@
+export { default as getValueType } from '@h-utils/get-value-type';
+export { default as operateObject } from '@h-utils/operate-object';
+export { default as filterEmptyContent } from '@h-utils/filter-empty-content';
