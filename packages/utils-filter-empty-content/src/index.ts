002d008cfed2a5e63e2cfeb56f5be78e49a8a05c
george-hong <hcj147@vip.qq.com>
2023-04-07 14:11:21 +0800

    chore: add eslint

--- packages/utils-filter-empty-content/src/index.ts
+++ packages/utils-filter-empty-content/src/index.ts
@@ -3,16 +3,16 @@ import operateObject from '@h-utils/operate-object';
 
 /**
  * filter the content of array or object, bump the field which value is ''、undefined、null、{}、[]
- * @param {Any} target The value you want to filter,except object or array.
+ * @param {Object | Array} target The value you want to filter,except object or array.
  * @param {boolean} isDeep If field is an array of object,filter it or not.
  * @returns {null | Object | Array} filter result
  */
 const filterEmptyParams = (target: unknown, isDeep = false): unknown => {
     return operateObject(target, (key, value) => {
-        const isArray = Array.isArray(value)
-        const isObject = getValueType(value) === 'Object'
+        const isArray = Array.isArray(value);
+        const isObject = getValueType(value) === 'Object';
         if (isDeep && (isArray || isObject)) {
-            value = filterEmptyParams(value, isDeep)
+            value = filterEmptyParams(value, isDeep);
         }
         const isNeed = (
             (value !== '') &&
@@ -20,13 +20,13 @@ const filterEmptyParams = (target: unknown, isDeep = false): unknown => {
             (value !== null) &&
             (!isArray || (isArray && (value as Array<unknown>).length > 0)) &&
             (!isObject || (isObject && Object.keys(value as object).length > 0))
-        )
+        );
         return {
             isNeed,
             key,
             value
-        }
-    })
-}
-
+        };
+    });
+};
+// esli
 export default filterEmptyParams;
