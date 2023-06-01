25cdae56b0aad89070d820be60263bc9c2129178
george-hong <hcj147@vip.qq.com>
2023-06-01 23:35:58 +0800

    refactor: simplify typedoc config

--- typedoc.cjs
+++ typedoc.cjs
@@ -1,8 +1,11 @@
 const pathPrefix = `./packages/utils${ process.env.PROJECT_NAME ? `-${process.env.PROJECT_NAME}` : '' }`;
 
 module.exports = {
-  $schema: 'https://typedoc.org/schema.json',
-  includeVersion: true,
   entryPoints: [`${ pathPrefix }/src/index.ts`],
   out: `${ pathPrefix }/doc`,
+  $schema: 'https://typedoc.org/schema.json',
+  readme: 'none',
+  disableSources: true,
+  includeVersion: true,
+  plugin: ['typedoc-plugin-markdown'],
 };
